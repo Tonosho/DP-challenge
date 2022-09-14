22cb877a9a2c21b553c5af5d4c0294508a538532
Tonosho <102292299+Tonosho@users.noreply.github.com>
2022-09-14 14:49:22 +0200

    version d'envoi

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Card.css';
 
 const Card = (props) => {
-  
+
   // Sélection / Déselection d'un film  
   const handleClick = (movieName, movieCategory) => {
     props.clickToSelectParent(movieName, movieCategory);
@@ -13,7 +13,7 @@ const Card = (props) => {
       <p className="Card-Title">{props.movieName}</p>
       <img src={props.movieUrl} alt={props.movieName} className="Image"></img>
       <div
-        className="Button"
+        className={props.status === "Selected" ? "Selected-button" : "Button"}
         onClick={() => handleClick(props.movieName, props.movieCategory)}>
         {props.status === "Selected" ? "Selected!" : "Choose"}
       </div>
